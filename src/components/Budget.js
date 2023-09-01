83a6f468fdd5e2b79d1ceaeacd4b2a1c9110167b
Alex Strigelskiy <avstri@gmail.com>
2023-09-01 18:48:59 -0400

    react final project excercise

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,13 @@
 
+import React, {useContext} from 'react';
+import {AppContext} from '../context/AppContext';
+const Budget = () => {
+    const {currency,budget} = useContext(AppContext);
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: {currency}{budget}</span>
+        </div>
+    );
+};
+
+export default Budget;
